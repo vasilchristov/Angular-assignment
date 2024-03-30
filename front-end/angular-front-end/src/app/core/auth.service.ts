5215a5c873725c974e64bebd5e0a88b05e404633
Vasil Christov <vasilchristov@yahoo.co.uk>
2024-03-30 18:04:11 +0200

    added login logout state

--- front-end/angular-front-end/src/app/core/auth.service.ts
+++ front-end/angular-front-end/src/app/core/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject} from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,31 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   private apiUrl = 'http://localhost:8081/api/auth';
+  private loggedInStatus = new BehaviorSubject<boolean>(this.hasToken());
 
   constructor(private http: HttpClient) { }
 
+  private hasToken(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+
   login(credentials: { email: string, password: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, credentials);
+    return this.http.post(`${this.apiUrl}/login`, credentials).pipe(
+      tap(response => {
+        this.loggedInStatus.next(true);
+      })
+    );;
+  }
+
+  get isLoggedIn() {
+    return this.loggedInStatus.asObservable();
+  }
+
+  logout(): void {
+    this.loggedInStatus.next(false);
   }
 
   register(user: any): Observable<any> {
       return this.http.post(`${this.apiUrl}/register`, user);
   }
-
 }
--- front-end/angular-front-end/src/app/shared/header/header.component.ts
+++ front-end/angular-front-end/src/app/shared/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,34 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isLoggedIn: boolean;
+  private authSubscription: Subscription = new Subscription();
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     const email = localStorage.getItem('email');
     this.isLoggedIn = !!email;
+  }
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.isLoggedIn.subscribe(
+      status => this.isLoggedIn = status
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+
+  checkLoginStatus(): void {
+    const authToken = localStorage.getItem('authToken');
+    this.isLoggedIn = !!authToken;
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('email');
+    this.isLoggedIn = false;
+    this.router.navigate(['/home']);
   }
 }
