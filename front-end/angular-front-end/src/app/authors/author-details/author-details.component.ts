9903aa1efc1d3dd9ea0c732c4d725077e33f181f
Vasil Christov <vasilchristov@yahoo.co.uk>
2024-03-31 22:26:28 +0300

    list articles of a particular author

--- front-end/angular-front-end/src/app/authors/author-details/author-details.component.ts
+++ front-end/angular-front-end/src/app/authors/author-details/author-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthorsService } from '../authors.service';
 import { Author } from '../authors.model';
+import { BlogPost } from 'src/app/blog/blog.model';
+import { BlogService } from 'src/app/blog/blog.service';
 
 @Component({
   selector: 'app-author-details',
@@ -11,22 +13,35 @@ import { Author } from '../authors.model';
 export class AuthorDetailsComponent implements OnInit{
 
   author?: Author;
+  blogs: BlogPost[] = [];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private authorsService: AuthorsService,
+    private blogService: BlogService
   ) {}
 
   ngOnInit() {
     const authorId = this.route.snapshot.paramMap.get('id');
     if (authorId) {
       this.authorsService.getAuthorById(+authorId).subscribe({
-        next: (author) => this.author = author,
+        next: (author) => {
+          this.author = author;
+          this.fetchAuthorPosts(author.email);
+        },
         error: () => this.router.navigate(['/authors'])
       });
     } else {
       this.router.navigate(['/authors']);
     }
+  }
+
+  private fetchAuthorPosts(email: string) {
+    this.blogService.getUserPostsByEmail(email).subscribe(posts => {
+      this.blogs = posts;
+    }, error => {
+      console.error('Error fetching posts for user:', error);
+    });
   }
 }
