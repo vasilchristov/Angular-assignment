dca8e51f5879434f2978262d98e3f2b8d0cdb69d
Vasil Christov <vasilchristov@yahoo.co.uk>
2024-04-01 15:08:38 +0300

    fix for edit user problem

--- front-end/angular-front-end/src/app/authors/authors.service.ts
+++ front-end/angular-front-end/src/app/authors/authors.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Author } from './authors.model';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ import { Author } from './authors.model';
 export class AuthorsService {
 
   private apiUrl = 'http://localhost:8081/api/authors';
+
+  // needed when editing an author and component does not refresh
+  private currentAuthorSubject = new BehaviorSubject<Author | null>(null);
 
   constructor(private http: HttpClient) { }
 
@@ -20,11 +24,29 @@ export class AuthorsService {
     return this.http.get<Author>(`${this.apiUrl}/${id}`);
   }
 
-  getAuthorByName(name: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/byName/${name}`);
+  getAuthorByName(name: string): Observable<Author> {
+    return this.http.get<Author>(`${this.apiUrl}/byName/${name}`);
+  }
+
+  fetchAndUpdateAuthorByName(name: string): void {
+    this.getAuthorByName(name).subscribe(author => {
+      this.currentAuthorSubject.next(author);
+    }, error => console.error('Error fetching author details', error));
   }
 
   updateAuthor(authorId: number, authorData: Author): Observable<Author> {
-    return this.http.put<Author>(`${this.apiUrl}/${authorId}`, authorData);
+    return this.http.put<Author>(`${this.apiUrl}/${authorId}`, authorData).pipe(
+        tap(updatedAuthor => {
+            this.setCurrentAuthor(updatedAuthor);
+        })
+    );
+  }
+
+  get currentAuthor$(): Observable<Author | null> {
+    return this.currentAuthorSubject.asObservable();
+  }
+
+  setCurrentAuthor(author: Author | null): void {
+    this.currentAuthorSubject.next(author);
   }
 }
--- front-end/angular-front-end/src/app/user/dashboard/dashboard.component.ts
+++ front-end/angular-front-end/src/app/user/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { BlogPost } from 'src/app/blog/blog.model';
 import { Router } from '@angular/router';
 import { BlogService } from 'src/app/blog/blog.service';
 import { AuthorsService } from 'src/app/authors/authors.service';
-import { Author } from 'src/app/authors/authors.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,46 +13,37 @@ export class DashboardComponent implements OnInit{
 
   blogs: BlogPost[] = [];
   authorName: string | undefined;
-  author: Author | undefined;
+  author$ = this.authorsService.currentAuthor$;
 
   constructor(
     private blogService: BlogService, 
     private router: Router,
     private authorsService: AuthorsService
-    ) { }
-
-  ngOnInit(): void {
-    const userEmail = localStorage.getItem('email');
-    if (userEmail) {
-      this.blogService.getUserPostsByEmail(userEmail).subscribe(posts => {
-        this.blogs = posts;
-        if (posts && posts.length > 0) {
-          this.authorName = posts[0].authorName;
-        }
-      }, error => {
-        console.error('Error fetching posts for user:', error);
-      });
-    } else {
-      console.warn('No user email found in localStorage');
+    ) { 
+      this.author$ = this.authorsService.currentAuthor$;
     }
 
-    const userName = localStorage.getItem('name');
-    if (userName) {
-      this.authorsService.getAuthorByName(userName).subscribe({
-        next: (author) => {
-          this.author = author;
-        },
-        error: () => console.error('Error fetching author details')
-      });
-    }
-  }
+    ngOnInit(): void {
+      const userEmail = localStorage.getItem('email');
+      if (userEmail) {
+        this.blogService.getUserPostsByEmail(userEmail).subscribe(posts => {
+          this.blogs = posts;
+        }, error => {
+          console.error('Error fetching posts for user:', error);
+        });
+      }
   
+      const userName = localStorage.getItem('name');
+      if (userName) {
+        this.authorsService.fetchAndUpdateAuthorByName(userName);
+      }
+    }
 
-  editPost(postId: number): void {
-    this.router.navigate(['/edit-blog', postId]);
-  }
+    editPost(postId: number): void {
+      this.router.navigate(['/edit-blog', postId]);
+    }
 
-  viewDetails(postId: number): void {
-    this.router.navigate(['/blog-details', postId]);
-  }
+    viewDetails(postId: number): void {
+      this.router.navigate(['/blog-details', postId]);
+    }
 }
